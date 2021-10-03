a224a327464d8f12230bf656ca7e8d04cc74258b
Ruijin <48195969+rjin1@users.noreply.github.com>
2021-10-03 19:17:58 -0400

    Add files via upload

---
+++ ComponentPlot.m
@@ -0,0 +1,16 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Map 7668 voxels back to 10000 voxels domain, and plot components%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+addpath('./Results')
+addpath('./Plot_Code')
+addpath('./fMRI_data')
+load brain_mask_conference.mat ind_brain
+
+% Load your estimates, for example:
+load test_z_earlystop.mat test_z
+
+% Start mapping and ploting
+SM = zeros(16,10000);
+SM(:,ind_brain) = test_z';
+
+showSMsyn_modified(SM,[2,8],0);
