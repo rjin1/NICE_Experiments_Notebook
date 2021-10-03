f85e5df31fd650f335236c55d737b5218834f62c
Ruijin <48195969+rjin1@users.noreply.github.com>
2021-10-03 19:22:38 -0400

    Update ComponentPlot.m

--- ComponentPlot.m
+++ ComponentPlot.m
@@ -1,8 +1,8 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Map 7668 voxels back to 10000 voxels domain, and plot components%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-addpath('./Results')
-addpath('./Plot_Code')
+addpath('./Results_synfMRI')
+addpath('./PlotCode')
 addpath('./SynfMRIspatialmaps')
 load brain_mask_conference.mat ind_brain
 
